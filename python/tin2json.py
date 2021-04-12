3a6a8337bf214bd3571fbaa83c1033df6687aeb4
Pratyush Kumar <pratyushkmr13@gmail.com>
2021-04-12 14:42:16 +0200

    file to convert obj terrain to CityJSON TINreleif

---
+++ python/tin2json.py
@@ -0,0 +1,65 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import geojson
+import os 
+# %%
+
+def write_CityJSON(json_dic):
+    file_path = os.path.join("../_data", "terra.json")
+    fh = open(file_path, "w+")#, encoding='utf-8')
+    json.dump(json_dic, fh, indent=2)#, ensure_ascii=False, indent=4)
+    fh.close()
+    print("voila! json updated")
+
+
+cj_dict= {  
+        "type": "CityJSON",
+        "version": "1.0",
+        "metadata": {
+              "referenceSystem":{
+                "type":"string",
+                "pattern":"EPSG::7415$"
+                }
+              },
+        "CityObjects": { # TODO: fill in building dictionaries here
+                        
+                       },
+        "vertices": []
+        }
+
+v_lst = []
+f_lst= []
+#  read obj
+# count = -1
+with open( '../_data/terra.obj', 'r') as fh:
+        for line in fh.readlines():
+            
+                count+=1
+                if line.startswith("v") : 
+                       xyz = line.split()[1:]
+                       v_lst.append(xyz)
+                if line.startswith("f"):
+                        fxyz = line.split()[1:]
+                        f_lst.append([fxyz])
+
+f_lst                        
+# %%
+
+dic = {
+        "type": "TINRelief", 
+  
+  "geometry": [{
+    "type": "CompositeSurface",
+    "lod": 1,
+    "boundaries": v_lst
+  }]    
+}
+
+cj_dict["vertices"] = v_lst
+cj_dict["CityObjects"].update({ "ground_only" : dic })
+
+# %%
+write_CityJSON(cj_dict)
+# %%
